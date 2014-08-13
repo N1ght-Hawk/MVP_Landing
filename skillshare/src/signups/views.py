d2a365a9e388b72c6a98161c0fd861b952900793
prabhathk <prabhath14411@gmail.com>
2014-08-13 23:01:19 +0530

    Edited CSS to say thanks for joining

--- skillshare/src/signups/views.py
+++ skillshare/src/signups/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,render_to_response,RequestContext
-
+from django.contrib import messages
 from .forms import signupform
 
 # Create your views here.
@@ -8,4 +8,5 @@ def home(request):
     if form.is_valid():
         save_it=form.save(commit=False)
         save_it.save()
+        messages.success(request,"Thank you for joining")
     return render_to_response("signup.html",locals(),context_instance=RequestContext(request))

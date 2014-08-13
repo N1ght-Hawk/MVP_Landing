45d12bda46fbc87a89bcf8d26a634118d4ee1423
prabhathk <prabhath14411@gmail.com>
2014-08-14 00:38:40 +0530

    Changed Navigation bar and thankyou pages

--- skillshare/src/mvp_landing/urls.py
+++ skillshare/src/mvp_landing/urls.py
@@ -7,9 +7,10 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'signups.views.home', name='home'),
+    url(r'^$', 'signups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^thank-you/$', 'signups.views.thankyou', name='thankyou'),
+    url(r'^about-us/$', 'signups.views.aboutus', name='aboutus'),
     url(r'^admin/', include(admin.site.urls)),
 )
 if settings.DEBUG:
